b195516345b5349c7f31821a5289324282d03c84
Keshti Gyllinger <gyllinger@gmail.com>
2025-04-15 16:42:04 +0200

    Add project files.

---
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    class Program
+    {
+        /// <summary>
+        /// The main method, vill handle the menues for the program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
+                    + "\n1. Examine a List"
+                    + "\n2. Examine a Queue"
+                    + "\n3. Examine a Stack"
+                    + "\n4. CheckParenthesis"
+                    + "\n0. Exit the application");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+                        ExamineList();
+                        break;
+                    case '2':
+                        ExamineQueue();
+                        break;
+                    case '3':
+                        ExamineStack();
+                        break;
+                    case '4':
+                        CheckParanthesis();
+                        break;
+                    /*
+                     * Extend the menu to include the recursive 
+                     * and iterative exercises.
+                     */
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Examines the datastructure List
+        /// </summary>
+        static void ExamineList()
+        {
+            bool done = false;
+            List<string> theList = new();
+
+            Console.WriteLine("Add or remove an item from the list by writing '+ your item' or '- your item'.");
+            Console.WriteLine("To exit, press 'Q'");
+
+            do
+            {
+                Console.Write("Enter item: ");
+
+                string input = Console.ReadLine() ?? "";
+
+                // Assign the first character to nav and convert it to
+                // lowercase (to handle both 'Q' and 'q')
+                char nav = char.ToLower(input[0]);
+
+                // Get the rest of the input and trim any whitespace
+                string value = input.Substring(1).Trim();
+
+                switch (nav)
+                {
+                    case '+':
+                        theList.Add(value);
+                        Console.WriteLine($"Added {value} to the list.");
+
+                        // Show how many items the list currently has
+                        Console.WriteLine($"List length: {theList.Count}");
+
+                        // Show how many items the list can hold before it needs to resize
+                        Console.WriteLine($"List capacity: {theList.Capacity}{Environment.NewLine}");
+
+                        break;
+
+                    case '-':
+                        // Try to remove the item with the entered value from the list
+                        if (theList.Remove(value))
+                        {
+                            Console.WriteLine($"Removed {value} from the list.");
+
+                            // Show how many items the list currently has
+                            Console.WriteLine($"List length: {theList.Count}");
+
+                            // Show how many items the list can hold before it needs to resize
+                            Console.WriteLine($"List capacity: {theList.Capacity}{Environment.NewLine}");
+                        }
+
+                        // If the item was not found, inform the user
+                        else
+                        {
+                            Console.WriteLine($"{value} not found in the list.");
+                        }
+                        break;
+
+                    case 'q':
+                        done = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please use only + or - to add or remove items from the list.");
+                        break;
+                }
+
+            }
+            while (!done);
+        }
+
+        /// <summary>
+        /// Examines the datastructure Queue
+        /// </summary>
+        static void ExamineQueue()
+        {
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch with cases to enqueue items or dequeue items
+             * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
+            */
+        }
+
+        /// <summary>
+        /// Examines the datastructure Stack
+        /// </summary>
+        static void ExamineStack()
+        {
+            /*
+             * Loop this method until the user inputs something to exit to main menue.
+             * Create a switch with cases to push or pop items
+             * Make sure to look at the stack after pushing and and poping to see how it behaves
+            */
+        }
+
+        static void CheckParanthesis()
+        {
+            /*
+             * Use this method to check if the paranthesis in a string is Correct or incorrect.
+             * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
+             * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
+             */
+
+        }
+
+    }
+}
+
